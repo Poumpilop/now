c9e16717ae3d1612ef53a600a064f6b1a7131644
Poumpilop <nesquiik3@sfr.fr>
2024-10-11 05:14:07 +0200

    affichage des posts / envoie

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,24 @@
+import Post from "@/components/posts/editor/Post";
+import PostEditor from "@/components/posts/editor/PostEditor";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: {createdAt: "desc"}
+  })
+
   return (
-    <main>front page</main>
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        {posts.map(post => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    </main>
   );
 }
